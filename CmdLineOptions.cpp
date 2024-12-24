39cf2b94827f8a20e6c2e52cae940ef4f618380f
Sergei.Shibanov <sergei.shibanov@aspose.com>
2024-12-24 22:26:46 +0500

    Maked output to stream for all params in CmdLineOptions

--- CmdLineOptions.cpp
+++ CmdLineOptions.cpp
@@ -74,15 +74,15 @@ bool CmdLineOptions::FillFromArgs(int argc, char** argv)
 std::ostream& CmdLineOptions::operator<<(std::ostream& os)
 {
     os << "include-dirs: "; 
-    out_string_s(os, _includedDirs);
-    os << std::endl;
-
-        //<< "exclude-dirs: " << out_string_s(os, _excludedDirs) << "\n"
-        //<< "depth: " << (short)_scanLevel << "\n"
-        //<< "min-size: " << _minFileSize << "\n"
-        //<< "file-masks: " << out_string_s(os, _fileMasks) << "\n"
-        //<< "block-size: " << _blockSize << "\n"
-        //<< "algorithm: " << (short)_hashAlgorithm << std::endl;
+    out_string_s(os, _includedDirs) << std::endl
+        << "exclude-dirs: ";
+    out_string_s(os, _excludedDirs) << std::endl
+        << "depth: " << (short)_scanLevel << std::endl
+        << "min-size: " << _minFileSize << std::endl
+        << "file-masks: ";
+    os << out_string_s(os, _fileMasks) << std::endl
+        << "block-size: " << _blockSize << std::endl
+        << "algorithm: " << (short)_hashAlgorithm << std::endl;
 
     return os;
 }
