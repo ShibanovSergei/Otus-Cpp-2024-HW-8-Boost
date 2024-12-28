b6a0165fd09eb3fa5af013d1a6a1858301f7754f
Sergei.Shibanov <sergei.shibanov@aspose.com>
2024-12-28 13:22:09 +0500

    Change __int8 to short for one parameter in CmdLineOptions::FillFromArgs

--- CmdLineOptions.cpp
+++ CmdLineOptions.cpp
@@ -9,7 +9,7 @@ bool CmdLineOptions::FillFromArgs(int argc, char** argv)
 {
     try
     {
-        auto checkDepthValue = [](const __int8& value)
+        auto checkDepthValue = [](const short& value)
             {
                 if (value != 0 && value != 1) throw po::validation_error(po::validation_error::invalid_option_value, "depth");
             };
@@ -33,7 +33,7 @@ bool CmdLineOptions::FillFromArgs(int argc, char** argv)
             ("help,h", "help message")
             ("include-dirs,i", po::value<string_s>()->default_value(string_s{ "." }, ".")->multitoken()->composing(), "Including to search directories, can be multiple. Default is current folder")
             ("exclude-dirs,e", po::value<string_s>()->multitoken()->composing(), "Excluded to search directories, can be multiple")
-            ("depth,d", po::value<__int8>()->default_value(0)->notifier(checkDepthValue), "Scan depth, 1 - all directories, 0 - current folder only. Default value is 0")
+            ("depth,d", po::value<short>()->default_value(0)->notifier(checkDepthValue), "Scan depth, 1 - all directories, 0 - current folder only. Default value is 0")
             ("min-size,m", po::value<int>()->default_value(1)->notifier(checkSizeValue), "Minimum size of the processed file in bytes. Default value is 1")
             ("file-masks,f", po::value<string_s>()->multitoken()->composing(), "Masks for the files participating in comparison")
             ("block-size,b", po::value<int>()->default_value(256)->notifier(checkSizeValue), "The size in bytes of the block to read the files with. Default value is 256")
@@ -54,7 +54,7 @@ bool CmdLineOptions::FillFromArgs(int argc, char** argv)
         if (vm.count("file-masks"))
             _fileMasks = vm["file-masks"].as<string_s>();
 
-        _scanLevel = ScanLevel(vm["depth"].as<__int8>());
+        _scanLevel = ScanLevel(vm["depth"].as<short>());
         _minFileSize = vm["min-size"].as<int>();
         _blockSize = vm["block-size"].as<int>();
         
